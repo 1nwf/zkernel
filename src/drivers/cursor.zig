68734208c529bf9613bd55bebf1ce72492314068
Nawaf <nwf.aloufi@gmail.com>
2023-02-15 10:53:32 -0500

    added sendVgaData function to write data to vga registers

--- src/drivers/cursor.zig
+++ src/drivers/cursor.zig
@@ -13,29 +13,28 @@ const cursor_end: u16 = 0x0B;
 const cursor_location_high: u8 = 0x0E;
 const cursor_location_low: u8 = 0x0F;
 
+/// writes register value to CRTC address register and value to CRTC data register
+fn sendVgaData(register: u8, value: anytype) void {
+    io.out(address, register);
+    io.out(data, value);
+}
+
 /// start and end between 0-15. start can't be greater than end or cursor will be hidden
 pub fn enableCursor(start: u8, end: u8) void {
-    io.out(address, cursor_start);
-    io.out(data, start);
-
-    io.out(address, cursor_end);
-    io.out(data, end);
+    sendVgaData(cursor_start, start);
+    sendVgaData(cursor_end, end);
 
     setLocation(0);
 }
 
 pub fn disableCursor() void {
-    io.out(address, cursor_start);
-    io.out(data, @as(u8, 0x20));
+    sendVgaData(cursor_start, @as(u8, 0x20));
 }
 
 pub fn setLocation(pos: u16) void {
     const upper: u8 = @truncate(u8, (pos & 0xFF00) >> 8);
     const lower: u8 = @truncate(u8, pos & 0x00FF);
 
-    io.out(address, cursor_location_high);
-    io.out(data, upper);
-
-    io.out(address, cursor_location_low);
-    io.out(data, lower);
+    sendVgaData(cursor_location_high, upper);
+    sendVgaData(cursor_location_low, lower);
 }
