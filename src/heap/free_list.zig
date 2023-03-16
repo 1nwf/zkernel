3f59145cfec2d1cd4522cc39fec197870a3adcdd
Nawaf <nwf.aloufi@gmail.com>
2023-03-15 21:37:28 -0400

    merge free list nodes if they are close together and adjust alloc size to fit a list node.

--- src/heap/free_list.zig
+++ src/heap/free_list.zig
@@ -2,24 +2,26 @@ const std = @import("std");
 const Tuple = std.meta.Tuple;
 const Allocator = std.mem.Allocator;
 const Error = Allocator.Error;
+const vga = @import("../drivers/vga.zig");
 
 pub const Node = struct {
     size: usize,
     next: ?*Node,
-    const NodeSize = @sizeOf(?*Node);
+    const NodeSize = @sizeOf(Node);
     fn init(size: usize, next: ?*Node) Node {
         return Node{ .size = size, .next = next };
     }
 
     fn take(self: *Node, comptime T: type) Tuple(&.{ ?*Node, *T }) {
-        const size = @sizeOf(T);
-        const value_ptr = @intToPtr(*T, @ptrToInt(self));
-        if (size < Node.NodeSize) {
+        // adjust the size to make sure it fits a List Node
+        const size = @max(@sizeOf(T), Node.NodeSize);
+        const value_ptr = @ptrCast(*T, self);
+        if (self.size < size) {
             return .{ null, value_ptr };
         }
         const free_space = self.size - size;
         var next = self.next;
-        const new_node = @intToPtr(*Node, @ptrToInt(self) + size);
+        const new_node = @intToPtr(*Node, self.address() + size);
         new_node.* = Node.init(free_space, null);
         new_node.next = next;
 
@@ -28,6 +30,27 @@ pub const Node = struct {
 
     inline fn address(self: *Node) usize {
         return @ptrToInt(self);
+    }
+
+    inline fn end_address(self: *Node) usize {
+        return self.address() + self.size;
+    }
+
+    fn insert(self: *Node, node: *Node) *Node {
+        if (self.end_address() == node.address()) {
+            self.size += node.size;
+            return self;
+        } else if (node.end_address() == self.address()) {
+            node.size += self.size;
+            return node;
+        } else if (self.address() < node.address()) {
+            node.next = self.next;
+            self.next = node;
+            return self;
+        } else {
+            node.next = self;
+            return node;
+        }
     }
 };
 
@@ -45,7 +68,7 @@ pub fn init(heap_start: usize, heap_size: usize) FreeList {
 pub fn alloc(self: *FreeList, comptime T: type, value: T) Error!*T {
     var prev: ?*Node = null;
     var node = self.head;
-    var size: usize = @sizeOf(T);
+    var size: usize = @max(Node.NodeSize, @sizeOf(T));
     while (node) |n| {
         if (n.size >= size) {
             break;
@@ -78,8 +101,9 @@ pub fn alloc(self: *FreeList, comptime T: type, value: T) Error!*T {
 }
 
 pub fn free(self: *FreeList, ptr: anytype) void {
-    const size = @sizeOf(@TypeOf(ptr));
-    const node = @intToPtr(*Node, @ptrToInt(ptr));
+    // size is guaranteed to be at least the size of a list Node
+    const size = @max(@sizeOf(@TypeOf(ptr.*)), Node.NodeSize);
+    const node = @ptrCast(*Node, ptr);
     node.* = Node.init(size, null);
     self.insert(node);
 }
@@ -95,14 +119,23 @@ fn insert(self: *FreeList, node: *Node) void {
             prev = n;
             curr_node = n.next;
         }
+
         if (prev) |p| {
-            node.next = p.next;
-            p.next = node;
+            _ = p.insert(node);
         } else {
-            node.next = self.head;
-            self.head = node;
+            self.head = node.insert(head);
         }
     } else {
         self.head = node;
     }
+}
+
+pub fn nodes(self: *FreeList) void {
+    var node = self.head;
+    vga.writeln("nodes------------", .{});
+    while (node) |n| {
+        vga.writeln("{*} @ {}", .{ n, n });
+        node = n.next;
+    }
+    vga.writeln("--------------", .{});
 }
--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,12 @@
 const vga = @import("drivers/vga.zig");
 const int = @import("interrupts/idt.zig");
 const timer = @import("interrupts/timer.zig");
+const heap = @import("heap/heap.zig");
 
 // kernel entry
 // has custom .entry section that is placed first in the .text section
-export fn entry() linksection(".entry") void {
-    main();
+export fn entry() void {
+    main() catch {};
 }
 
 fn halt() noreturn {
@@ -15,17 +16,32 @@ fn halt() noreturn {
     }
 }
 
-fn main() noreturn {
+var HEAP_START: u32 = 0x10000;
+const HEAP_SIZE: u32 = 108 * 1024; // 100 Kib
+const HEAP_END = HEAP_START + HEAP_SIZE;
+
+fn main() !noreturn {
     vga.init(.{ .bg = .LightRed, .fg = .White }, .Underline);
     int.enable();
     int.init();
     int.load();
 
-    var secs: u8 = 0;
-    while (secs < 10) : (secs += 1) {
-        timer.wait(1);
-        vga.write("{} ", .{secs + 1});
-    }
+    var fl = heap.FreeListAllocator.init(HEAP_START, HEAP_SIZE);
+    const val = try fl.alloc(u32, 12);
+
+    vga.writeln("val1 {*} {}", .{ val, val.* });
+
+    fl.nodes();
+    var val2 = try fl.alloc(u64, 20);
+    vga.writeln("val2: {*} {}", .{ val2, val2.* });
+
+    fl.nodes();
+
+    fl.free(val2);
+    fl.nodes();
+
+    fl.free(val);
+    fl.nodes();
 
     halt();
 }
