df9945f386cf988585cca8a9841396ac88c187ad
Nawaf <nwf.aloufi@gmail.com>
2023-04-09 01:57:31 -0400

    read bootinfo from bootloader

--- src/main.zig
+++ src/main.zig
@@ -15,13 +15,31 @@ var HEAP_START: u32 = 0x10000;
 const HEAP_SIZE: u32 = 108 * 1024; // 100 Kib
 const HEAP_END = HEAP_START + HEAP_SIZE;
 
-export fn main() void {
+const BootInfo = struct { mapAddr: u32, size: u32 };
+
+const MemMapEntry = extern struct {
+    base: u64,
+    length: u64,
+    type: u32,
+    acpi: u32,
+};
+
+export fn main(bootInfoAddr: u32) void {
     int.init();
     int.load();
 
     vga.init(.{ .bg = .LightRed, .fg = .White }, .Underline);
 
-    vga.writeln("Kernel Loaded", .{});
+    vga.writeln("boot info addr {x}", .{bootInfoAddr});
+
+    const bootInfo = @intToPtr(*BootInfo, bootInfoAddr);
+
+    vga.writeln("ptr is {}", .{bootInfo});
+    var map = @intToPtr([]MemMapEntry, bootInfo.mapAddr);
+
+    for (0..bootInfo.size) |i| {
+        vga.writeln("{}", .{map[i]});
+    }
 
     halt();
 }
