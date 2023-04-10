bb0fb763c54683ecc9f7137a009c7b9f1f22247f
Nawaf <nwf.aloufi@gmail.com>
2023-04-10 08:37:45 -0400

    mem map

---
+++ src/cpu/paging/memmap.zig
@@ -0,0 +1,21 @@
+const Entry = struct {
+    start: u32,
+    size: u32,
+    regionType: RegionType,
+    fn init(start: u32, size: u32, regionType: RegionType) Entry {
+        return Entry{ .start = start, .size = size, .regionType = regionType };
+    }
+};
+
+const RegionType = enum {
+    Kernel,
+    Used,
+    Available,
+};
+
+pub const SMAPEntry = extern struct {
+    base: u64,
+    length: u64,
+    type: u32,
+    acpi: u32,
+};
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const int = @import("interrupts/idt.zig");
 const timer = @import("interrupts/timer.zig");
 const heap = @import("heap/heap.zig");
 const pg = @import("cpu/paging/paging.zig");
+const mem = @import("cpu/paging/memmap.zig");
 
 fn halt() noreturn {
     asm volatile ("sti");
@@ -17,13 +18,7 @@ const HEAP_END = HEAP_START + HEAP_SIZE;
 
 const BootInfo = struct { mapAddr: u32, size: u32 };
 
-const MemMapEntry = extern struct {
-    base: u64,
-    length: u64,
-    type: u32,
-    acpi: u32,
-};
-
+pub var bootInfo = BootInfo{ .mapAddr = 0, .size = 0 };
 export fn main(bootInfoAddr: u32) void {
     int.init();
     int.load();
@@ -32,10 +27,10 @@ export fn main(bootInfoAddr: u32) void {
 
     vga.writeln("boot info addr {x}", .{bootInfoAddr});
 
-    const bootInfo = @intToPtr(*BootInfo, bootInfoAddr);
+    bootInfo = @intToPtr(*BootInfo, bootInfoAddr).*;
 
     vga.writeln("ptr is {}", .{bootInfo});
-    var map = @intToPtr([]MemMapEntry, bootInfo.mapAddr);
+    var map = @intToPtr([]mem.SMAPEntry, bootInfo.mapAddr);
 
     for (0..bootInfo.size) |i| {
         vga.writeln("{}", .{map[i]});
