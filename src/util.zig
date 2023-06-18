f455e1edac2fc9a093bf8207dcd23e73d7b7e225
Nawaf <nwf.aloufi@gmail.com>
2023-06-18 19:24:51 -0400

    fix stepBy function

--- src/util.zig
+++ src/util.zig
@@ -8,20 +8,20 @@ pub const StepIterator = struct {
     pub fn init(start: u64, end: u64, step_size: u64) StepIterator {
         return StepIterator{ .start = start, .end = end, .step_size = step_size, .next_val = null };
     }
-
     pub fn next(self: *StepIterator) ?u64 {
-        if (self.next_val == null) {
-            self.next_val = self.start;
-            return self.next_val;
-        }
-        const nextVal = self.next_val.? + self.step_size;
-        if (nextVal > self.end) {
-            self.next_val = null;
-            return null;
-        }
+        if (self.next_val) |val| {
+            const nextVal = val + self.step_size;
+            if (nextVal > self.end) {
+                self.next_val = null;
+                return null;
+            }
 
-        self.next_val = nextVal;
-        return self.next_val;
+            self.next_val = nextVal;
+            return val;
+        } else {
+            self.next_val = self.start + self.step_size;
+            return self.start;
+        }
     }
 };
 pub fn stepBy(start: u64, stop: u64, step: u64) StepIterator {
