9a4173b91f91df1d30df0d856f92e8842d87b59f
Nawaf <nwf.aloufi@gmail.com>
2023-08-15 22:56:56 -0400

    fix bump allocator algorithm and add tests

--- src/vmm/bump_alloc.zig
+++ src/vmm/bump_alloc.zig
@@ -1,12 +1,15 @@
-const MemMapEntry = @import("../../boot/mutliboot_header.zig").MemMapEntry;
+const MemMapEntry = @import("../boot/mutliboot_header.zig").MemMapEntry;
+const MemoryRegion = @import("memmap.zig").MemRegion;
+const PAGE_SIZE = @import("arch").paging.PAGE_SIZE;
 const std = @import("std");
+
 const ListNodePtr = *allowzero ListNode;
 const ListNode = struct {
     size: usize,
     next: ?ListNodePtr = null,
 
-    fn init(addr: usize, size: usize) ListNodePtr {
-        var node: ListNodePtr = @ptrFromInt(addr);
+    fn init(addr: *anyopaque, size: usize) ListNodePtr {
+        var node: ListNodePtr = @ptrCast(@alignCast(addr));
         node.size = size;
         node.next = null;
         return node;
@@ -21,15 +24,35 @@ const ListNode = struct {
         const other_end = other_addr + other.size;
         if (self_end == other_addr or other_end == self_addr) {
             const start = @min(self_addr, other_addr);
-            return ListNode.init(start, self.size + other.size);
+            const next1 = self.next;
+            const next2 = other.next;
+            const node = ListNode.init(@ptrFromInt(start), self.size + other.size);
+            if (next1) |n1| {
+                if (next2) |n2| {
+                    node.next = n1.appendNode(n2);
+                } else {
+                    node.next = n1;
+                }
+            } else if (next2) |n2| {
+                node.next = n2;
+            }
+            return node;
         }
 
-        if (self_addr > other_addr) {
-            self.next = other;
+        if (self_addr < other_addr) {
+            if (self.next) |next| {
+                self.next = next.appendNode(other);
+            } else {
+                self.next = other;
+            }
             return self;
         }
 
-        other.next = self;
+        if (other.next) |next| {
+            other.next = next.appendNode(self);
+        } else {
+            other.next = self;
+        }
         return other;
     }
 };
@@ -38,17 +61,20 @@ const AllocError = error{
     OutOfMem,
 };
 
-const PAGE_SIZE = 4096;
 extern const kernel_start: usize;
 extern const kernel_end: usize;
 
 pub const BumpAllocator = struct {
     head: ?ListNodePtr = null,
-    var prev: ?ListNodePtr = null;
+    reserved: []MemoryRegion,
+    const Self = @This();
 
     // TODO: dont override kernel code or kenrel stack
-    pub fn init(memMap: []MemMapEntry) @This() {
-        var allocator = @This(){};
+    pub fn init(memMap: []MemMapEntry, reserved: []MemoryRegion) Self {
+        var allocator = Self{
+            .Reserved = reserved,
+        };
+        var prev: ?ListNodePtr = null;
         for (memMap) |entry| {
             if (entry.type != .Available or entry.length < PAGE_SIZE) continue;
             var base_addr = std.mem.alignForward(usize, @as(usize, @intCast(entry.base_addr)), PAGE_SIZE);
@@ -65,7 +91,7 @@ pub const BumpAllocator = struct {
         return allocator;
     }
 
-    pub fn allocPage(self: *@This()) AllocError![*]u8 {
+    pub fn allocPage(self: *Self) AllocError![*]u8 {
         if (self.head == null) {
             return AllocError.OutOfMem;
         }
@@ -77,7 +103,7 @@ pub const BumpAllocator = struct {
         return @ptrCast(head);
     }
 
-    pub fn freePage(self: *@This(), addr: usize) void {
+    pub fn freePage(self: *Self, addr: usize) void {
         if (self.head == null) {
             self.head = ListNode.init(addr, PAGE_SIZE);
             return;
@@ -85,7 +111,7 @@ pub const BumpAllocator = struct {
         self.head = self.head.?.appendNode(ListNode.init(addr, PAGE_SIZE));
     }
 
-    pub fn print(self: *@This()) void {
+    pub fn print(self: *Self) void {
         var temp = self.head;
         while (temp) |entry| {
             std.log.info("entry: 0x{x}", .{entry.size});
@@ -93,3 +119,18 @@ pub const BumpAllocator = struct {
         }
     }
 };
+
+test "ListNode.appendNode" {
+    const expectEqual = std.testing.expectEqual;
+    var buff: [3]ListNode = undefined;
+    var node = ListNode.init(&buff[0], @sizeOf(ListNode));
+    var node2 = ListNode.init(&buff[1], @sizeOf(ListNode));
+    var node3 = ListNode.init(&buff[2], @sizeOf(ListNode));
+
+    var head = node.appendNode(node3);
+    try expectEqual(node, head);
+    try expectEqual(head.next, node3);
+    head = node.appendNode(node2);
+    try expectEqual(node, head);
+    try expectEqual(head.next, node3);
+}
