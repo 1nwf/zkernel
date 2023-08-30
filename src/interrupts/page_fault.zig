923dced5aaa1fe4986e69cca4170ae8454a36342
Nawaf <nwf.aloufi@gmail.com>
2023-08-30 17:00:35 -0400

    update page fault handler

--- src/interrupts/page_fault.zig
+++ src/interrupts/page_fault.zig
@@ -3,9 +3,10 @@ const Context = @import("interrupts.zig").Context;
 
 // NOTE: address that caused page fault is stored in the `cr2` register
 pub export fn pageFaultHandler(ctx: Context) void {
+    _ = ctx;
     const cr2 = asm volatile (
         \\ mov %%cr2, %[cr2]
         : [cr2] "={eax}" (-> usize),
     );
-    std.log.info("a page fault occured: {} {}", .{ ctx.int_num, cr2 });
+    std.log.info("a page fault occured: 0x{x}", .{cr2});
 }
