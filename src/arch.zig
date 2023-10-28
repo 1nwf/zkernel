275372c1e644ac7cfed434c45a5d1c41324cf720
Nawaf <nwf.aloufi@gmail.com>
2023-10-27 23:03:09 -0400

    merge main

--- src/arch.zig
+++ src/arch.zig
@@ -2,7 +2,7 @@ pub const serial = @import("arch/serial.zig");
 pub const gdt = @import("arch/gdt.zig");
 pub const paging = @import("arch/paging/paging.zig");
 
-pub usingnamespace @import("arch/io.zig");
+pub const io = @import("arch/io.zig");
 
 test {
     _ = paging;
