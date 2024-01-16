b37fbea972a60a4b275273d8bc619cc13de6c229
Nawaf <nwf.aloufi@gmail.com>
2024-01-16 13:34:09 -0500

    rename running_queue

--- src/process/scheduler.zig
+++ src/process/scheduler.zig
@@ -4,27 +4,27 @@ const Thread = process.Thread;
 const arch = @import("arch");
 const Deque = @import("../utils/deque.zig").Deque;
 
-const RunningQueue = Deque(Thread, 14);
+const RunQueue = Deque(Thread, 14);
 
-running_queue: RunningQueue,
+run_queue: RunQueue,
 active_thread: ?Thread = null,
 var self: @This() = undefined;
 
 pub fn init(allocator: std.mem.Allocator) !void {
     self = .{
-        .running_queue = try RunningQueue.init(allocator),
+        .run_queue = try RunQueue.init(allocator),
     };
 }
 
 pub fn schedule_thread(thread: Thread) !void {
-    try self.running_queue.pushBack(thread);
+    try self.run_queue.pushBack(thread);
 }
 
 pub fn run_next() ?*arch.thread.Context {
     if (self.active_thread) |th| {
         schedule_thread(th) catch {};
     }
-    const next = self.running_queue.popFront() orelse return null;
+    const next = self.run_queue.popFront() orelse return null;
     self.active_thread = next;
     return &self.active_thread.?.context;
 }
