b816e52fc4f4a1b8639c0a4b41e4a11018cb309c
Nawaf <nwf.aloufi@gmail.com>
2024-01-18 18:36:39 -0500

    fix syscall bugs by setting clobbered registers in inline asm

--- stdlib/exit.zig
+++ stdlib/exit.zig
@@ -2,5 +2,5 @@ pub fn exit() void {
     asm volatile (
         \\ mov $1, %%eax
         \\ int $0x30
-    );
+        ::: "eax");
 }
--- stdlib/write.zig
+++ stdlib/write.zig
@@ -1,28 +1,26 @@
 const std = @import("std");
 const Writer = std.io.Writer(void, error{}, writeFn);
+const writer: Writer = .{ .context = {} };
 
-fn write(str: []const u8) void {
+fn write(str: *const []const u8) void {
     asm volatile (
         \\ xor %%eax, %%eax
         \\ int $48
         :
-        : [value] "{ebx}" (&str),
+        : [value] "{ebx}" (str),
+        : "eax"
     );
 }
 
 fn writeFn(_: void, str: []const u8) !usize {
-    write(str);
+    write(&str);
     return str.len;
 }
 
-fn writer() Writer {
-    return .{ .context = {} };
-}
-
 pub fn print(comptime str: []const u8, args: anytype) void {
-    std.fmt.format(writer(), str, args) catch {};
+    std.fmt.format(writer, str, args) catch {};
 }
 
 pub fn println(comptime str: []const u8, args: anytype) void {
-    std.fmt.format(writer(), str ++ "\n", args) catch {};
+    std.fmt.format(writer, str ++ "\n", args) catch {};
 }
